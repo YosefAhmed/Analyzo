aa2f50e7a5be2d59325e68a59a8826c34f91477f
Your Name <you@example.com>
2019-04-24 00:11:28 +0200

    Add project files.

---
+++ Analyzo/Start page.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Analyzo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Start_page : ContentPage
+    {
+        public Start_page()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Analyzo/search page.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Analyzo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class search_page : ContentPage
+    {
+        public search_page()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
